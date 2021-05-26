dfd8ab69ef30eaf022ac7cbf2e93a26f06dc668f
= <chris.filiatrault@hotmail.com>
2021-05-27 08:29:49 +1000

    Remove resume link

--- src/components/home/GetInTouch.jsx
+++ src/components/home/GetInTouch.jsx
@@ -6,9 +6,7 @@ const GetInTouch = ({ heading, message, linkedIn}) => {
     <>
       <h2 className="display-4 pb-3 text-center">{heading}</h2>
       <p className="lead text-center pb-3">
-        {message} <a href="https://www.linkedin.com/in/chris-filiatrault/" target="_blank">LinkedIn, </a> 
-        or view my public
-        <a href="https://1drv.ms/w/s!ArD4GThUJSRbmdgYRxG30i9me4D04w?e=8cPgLj" target="_blank"> resume </a>online.
+        {message} <a href="https://www.linkedin.com/in/chris-filiatrault/" target="_blank">LinkedIn.</a> 
       </p>
       
     </>
