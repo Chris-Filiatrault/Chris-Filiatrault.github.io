12f43a918e889d4e7bd459a0e9b289eb4513a90b
= <chris.filiatrault@hotmail.com>
2021-05-26 14:16:06 +1000

    Remove projects section

---
+++ build/precache-manifest.b117a970bccdb880fb1475080911a522.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e4ecae0bb885e2d0684e6728b7600d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65fb41a060d4ab64ee71",
+    "url": "/static/css/main.5d72f477.chunk.css"
+  },
+  {
+    "revision": "786d5de90ccb71783c89",
+    "url": "/static/js/2.894a586a.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.894a586a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65fb41a060d4ab64ee71",
+    "url": "/static/js/main.68670643.chunk.js"
+  },
+  {
+    "revision": "ab1a795d60b0cfbb7b47",
+    "url": "/static/js/runtime-main.38a8196a.js"
+  },
+  {
+    "revision": "7471d8e811c4c99dda81b34d9a840cf5",
+    "url": "/static/media/2021-green-lower-res.7471d8e8.jpg"
+  }
+]);
--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -52,7 +52,7 @@ const about = {
 // If you want to display specfic projects, add the repository names,
 //      i.e ["repository-1", "repo-2"]
 const repos = {
-  show: true,
+  show: false,
   heading: "Recent Projects",
   gitHubUsername: "Chris-filiatrault", //i.e."johnDoe12Gh"
   reposLength: 4,
