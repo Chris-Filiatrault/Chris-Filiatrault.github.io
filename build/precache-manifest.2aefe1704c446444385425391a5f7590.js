7ea60227920678b5c984ab46e008de9ebfe02ce2
= <chris.filiatrault@hotmail.com>
2021-06-01 18:01:45 +1000

    Add build

---
+++ build/precache-manifest.2aefe1704c446444385425391a5f7590.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "597e00e7bd1dd38bc990a4277a3fc0fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f0d253fbe8a8863a3c2",
+    "url": "/static/css/main.0e0025e2.chunk.css"
+  },
+  {
+    "revision": "31d74a95d87d1e8d1141",
+    "url": "/static/js/2.30aec82e.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.30aec82e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f0d253fbe8a8863a3c2",
+    "url": "/static/js/main.549a5e95.chunk.js"
+  },
+  {
+    "revision": "ab1a795d60b0cfbb7b47",
+    "url": "/static/js/runtime-main.38a8196a.js"
+  }
+]);
