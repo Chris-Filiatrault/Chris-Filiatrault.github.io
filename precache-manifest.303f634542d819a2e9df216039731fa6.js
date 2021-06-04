20866c75b6651ea6d24cf78d1c6ce076705bf643
= <chris.filiatrault@hotmail.com>
2021-06-04 11:43:46 +1000

    Updates

---
+++ precache-manifest.303f634542d819a2e9df216039731fa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaa46bbf7cb7d2784ada2491abd6ee21",
+    "url": "./index.html"
+  },
+  {
+    "revision": "29f9c63d4e4b9a1f4bd2",
+    "url": "./static/css/main.3804f821.chunk.css"
+  },
+  {
+    "revision": "6c08c7fbef02b0ec8621",
+    "url": "./static/js/2.63517aac.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.63517aac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29f9c63d4e4b9a1f4bd2",
+    "url": "./static/js/main.6145b857.chunk.js"
+  },
+  {
+    "revision": "247e09194dbbf25bc59f",
+    "url": "./static/js/runtime-main.126b5623.js"
+  }
+]);
