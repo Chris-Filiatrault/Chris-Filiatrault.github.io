ea989f51c7518ca30b45047532abf68df54822dc
= <chris.filiatrault@hotmail.com>
2021-06-04 11:35:23 +1000

    Update homepage, update readme, remove logo.svg, safely open LinkedIn link in new tab

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # <center> Personal Website </center>
 
-This repo hosts the source code for [chrisfiliatrault.com](https://chrisfiliatrault.com/)
+This repo hosts the source code for [https://chris-filiatrault.github.io](https://chris-filiatrault.github.io)
 
 Many thanks to Hashir Shoaib who made this open source template which is available [here](https://github.com/hashirshoaeb/home).
 
--- src/components/GetInTouch.jsx
+++ src/components/GetInTouch.jsx
@@ -6,7 +6,7 @@ const GetInTouch = ({ heading, message, linkedIn}) => {
     <>
       <h2 className="display-4 pb-3 text-center">{heading}</h2>
       <p className="lead text-center pb-3">
-        {message} <a href="https://www.linkedin.com/in/chris-filiatrault/" target="_blank">LinkedIn.</a> 
+        {message} <a href="https://www.linkedin.com/in/chris-filiatrault/" target="_blank" rel="noopener noreferrer">LinkedIn.</a> 
       </p>
       
     </>
